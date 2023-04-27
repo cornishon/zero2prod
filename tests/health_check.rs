0e1363be1e0224b3dab3639d331034b6c24ac7d8
Adam Zadrożny <zadroznyadam@pm.me>
2023-04-27 14:34:20 +0200

    refactor tests slightly

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,21 +3,22 @@ use std::net::TcpListener;
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app().await;
-    let endpoint = format!("http://{address}/health_check");
 
     let response = reqwest::Client::new()
-        .get(endpoint)
+        .get(format!("{address}/health_check"))
         .send()
         .await
-        .expect("request to {endpoint}");
+        .expect("request to health_check");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
+/// Spin up an instance of our application
+/// and returns its address (i.e. http://localhost:XXXX)
 async fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("to bind a random port");
     let port = listener.local_addr().unwrap().port();
     tokio::spawn(zero2prod::run(listener));
-    format!("127.0.0.1:{port}")
+    format!("http://127.0.0.1:{port}")
 }
