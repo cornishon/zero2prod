6e9f0267387c12cbadc20e26e194df11ca0e8311
Adam Zadrożny <zadroznyadam@pm.me>
2023-04-28 15:32:09 +0200

    clippy

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() {
     let config = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    let connection_pool = PgPool::connect(&config.database.connection_string().expose_secret())
+    let connection_pool = PgPool::connect(config.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
